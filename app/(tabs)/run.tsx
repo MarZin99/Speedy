ca838415a591a61cc7bd226ec4d1017c2cd5f0bb
Marcin Zawadzki <mar.zawadzki99@gmail.com>
2024-05-15 16:06:25 +0200

    Get location of user and display it on map

--- app/(tabs)/run.tsx
+++ app/(tabs)/run.tsx
@@ -1,11 +1,44 @@
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
+import { View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { useEffect, useState } from "react";
+
+import * as Location from "expo-location";
 
 export default function RunScreen() {
+  const [location, setLocation] = useState<Location.LocationObject>();
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status == "granted") {
+        console.log("Permission granted");
+      } else {
+        {
+          console.log("Permission not granted");
+        }
+      }
+
+      await Location.getCurrentPositionAsync()
+        .then((res) => setLocation(res))
+        .catch((err) => console.log(err));
+    })();
+  }, []);
+
+  // function to check permissions and get Locatio
   return (
     <>
-      <ThemedText type="title">Welcome</ThemedText>
-      <ThemedText>This app bla bla bla</ThemedText>
+      <View>
+        <MapView style={{ width: "100%", height: "90%" }}>
+          {location && (
+            <Marker
+              coordinate={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+              }}
+            ></Marker>
+          )}
+        </MapView>
+      </View>
     </>
   );
 }
