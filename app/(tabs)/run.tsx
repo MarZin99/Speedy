e2c52c25a18df7b690ec67c1e407f65bf8608cd1
Marcin Zawadzki <mar.zawadzki99@gmail.com>
2024-05-15 12:44:30 +0200

    Added blank page Run

---
+++ app/(tabs)/run.tsx
@@ -0,0 +1,11 @@
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+
+export default function RunScreen() {
+  return (
+    <>
+      <ThemedText type="title">Welcome</ThemedText>
+      <ThemedText>This app bla bla bla</ThemedText>
+    </>
+  );
+}
